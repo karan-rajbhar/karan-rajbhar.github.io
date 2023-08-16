d62380c08bdb9b3f809b264faebe8fdf211fb389
Karan Rajbhar <kran@outlook.com>
2023-08-16 09:56:36 +0530

    temp fix toggle button

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,8 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+//TODO ADD Hamburger for mobile use
+
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//     document.querySelector('.nav-links').classList.toggle('expanded');
+//   });
 
 
   
